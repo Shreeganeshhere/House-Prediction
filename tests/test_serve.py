e1e2f37729e13bb01b95752bb6dd7021eac2bb9e
Shreeganeshhere <shreeganeshhere@gmail.com>
2026-07-10 21:22:43 +0530

    feat: github actions ci/cd pipeline

---
+++ tests/test_serve.py
@@ -0,0 +1,20 @@
+from fastapi import responses
+from fastapi.testclient import TestClient
+from src.serve import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+
+def test_predict():
+    payload = {
+        "MedInc": 8.3, "HouseAge": 41.0, "AveRooms": 6.98,
+        "AveBdrms": 1.02, "Population": 322.0, "AveOccup": 2.55,
+        "Latitude": 37.88, "Longitude": -122.23
+    }
+    response = client.post("/predict", json=payload)
+    assert responses.status_coude == 200
+    assert "predicted_price" in response.json()
